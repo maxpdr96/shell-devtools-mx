6e9ed7859025f4e61567669611e0b6e65a9c2672
maxpdr <maxpdr96@gmail.com>
2025-12-16 21:42:20 -0300

    Generate UUIDs

--- src/main/java/com/hidarisoft/shelldevtoolsmx/generate/GenerateCommands.java
+++ src/main/java/com/hidarisoft/shelldevtoolsmx/generate/GenerateCommands.java
@@ -5,6 +5,10 @@ import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
 import org.springframework.shell.standard.ShellOption;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
 @ShellComponent
 public class GenerateCommands {
 
@@ -17,5 +21,20 @@ public class GenerateCommands {
     @ShellMethod(key = "gen cpf", value = "Generate CPF")
     public String generateCpf(@ShellOption(defaultValue = "1") int quantity) {
         return String.join("\n", cpfGeneratorService.generateListCpf(quantity));
+    }
+
+    @ShellMethod(key = "gen uuid", value = "Generate UUIDs")
+    public String generateUUID(@ShellOption(defaultValue = "1") int quantity) {
+
+        if (quantity == 1) {
+            return UUID.randomUUID().toString();
+        }
+
+        List<String> listUUIDs = new ArrayList<>();
+        for (int i = 0; i < quantity; i++) {
+            listUUIDs.add(UUID.randomUUID().toString());
+        }
+
+        return String.join("\n", listUUIDs);
     }
 }
